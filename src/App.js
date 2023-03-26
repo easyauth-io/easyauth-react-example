8396f3ecf611d324e878691a53fb3194f62f49f3
Bishakh Ghosh <ghoshbishakh@gmail.com>
2023-03-26 20:48:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import logo from './logo.svg';
+import './App.css';
+import { useAuth, hasAuthParams } from "react-oidc-context";
+import Profile from './Profile';
+import { useEffect } from 'react';
+function App() {
+  const auth = useAuth();
+  
+  // automatically sign-in
+  useEffect(() => {
+    if (!hasAuthParams() &&
+    !auth.isAuthenticated && !auth.activeNavigator && !auth.isLoading) {
+      auth.signinRedirect();
+    }
+  }, [auth.isAuthenticated, auth.activeNavigator, auth.isLoading, auth.signinRedirect]);
+  
+  
+  
+  switch (auth.activeNavigator) {
+    case "signinSilent":
+    return <div>Signing you in...</div>;
+    case "signoutRedirect":
+    return <div>Signing you out...</div>;
+  }
+  
+  if (auth.isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  
+  if (auth.error) {
+    return <div>Oops... {auth.error.message}</div>;
+  }
+  
+  if (auth.isAuthenticated) {
+    return (
+      <div className="App">
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+      Hello {auth.user?.profile.sub}{" "}
+      </p>
+      <Profile/>
+      <button style={{padding: "0.2em 1em",
+      "fontSize":"1em",
+      "border": "none",
+      "borderRadius": "2em",
+      "backgroundColor": "white",
+      "cursor": "pointer"}} onClick={() => void auth.removeUser()}>
+      Log out
+      </button>
+      </header>
+      </div>
+      );
+    }
+    
+    return (
+      <div className="App">
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+      Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <button style={{padding: "0.2em 1em",
+      "fontSize":"1em",
+      "border": "none",
+      "borderRadius": "2em",
+      "backgroundColor": "white",
+      "cursor": "pointer"}} onClick={() => void auth.signinRedirect()}>
+      Log in
+      </button>
+      </header>
+      </div>
+      );
+    }
+    
+    export default App;
+    
